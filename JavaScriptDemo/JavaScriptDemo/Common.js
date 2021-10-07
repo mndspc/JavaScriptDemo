f778926ef491cdb10e3b7cccddacaaa09bb76be1
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-10-07 10:59:21 +0530

    Push new HTML Pages

---
+++ JavaScriptDemo/JavaScriptDemo/Common.js
@@ -0,0 +1,6 @@
+﻿function PrintSum(cnt1, cnt2) {
+    var num1 = document.getElementById(cnt1).value;
+    var num2 = document.getElementById(cnt2).value;
+    var sum = parseInt(num1) + parseInt(num2);
+    document.getElementById("result").innerHTML = sum;
+}
